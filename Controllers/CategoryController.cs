0bfc3c6527768944b6f3f4346e031baa12b928d4
Habib Hussain <HabibHussain82@gmail.com>
2023-07-05 19:05:40 +0500

    Add project files.

---
+++ Controllers/CategoryController.cs
@@ -0,0 +1,119 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ProductMicroservice.Models;
+
+namespace ProductMicroservice.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoryController : ControllerBase
+    {
+        private readonly ProductDBContext _dbContext;
+
+        public CategoryController(ProductDBContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        // GET: api/<CategoryController>
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Category>>> Get()
+        {
+            if (_dbContext.Category == null)
+            {
+                return NotFound();
+            }
+            return await _dbContext.Category.ToListAsync();
+        }
+
+
+        // GET api/<CategoryController>/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Category>> Get(int id)
+        {
+            if (_dbContext.Category == null)
+            {
+                return NotFound();
+            }
+            var prod = await _dbContext.Category.FindAsync(id);
+
+            if (prod == null)
+            {
+                return NotFound();
+            }
+
+            return prod;
+        }
+
+        // POST api/<CategoryController>
+        [HttpPost]
+        public async Task<ActionResult<Category>> Post(Category category)
+        {
+            if (_dbContext.Category == null)
+            {
+                return Problem("Entity is null.");
+            }
+            _dbContext.Category.Add(category);
+            await _dbContext.SaveChangesAsync();
+
+            return CreatedAtAction("Get", new { id = category.Id }, category);
+        }
+
+
+        // PUT api/<CategoryController>/5
+        [HttpPut("{id}")]
+        public async Task<IActionResult> Put(int id, Category category)
+        {
+            if (id != category.Id)
+            {
+                return BadRequest();
+            }
+
+            _dbContext.Entry(category).State = EntityState.Modified;
+
+            try
+            {
+                await _dbContext.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!Exists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // DELETE api/<CategoryController>/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(int id)
+        {
+            if (_dbContext.Category == null)
+            {
+                return NotFound();
+            }
+            var Category = await _dbContext.Category.FindAsync(id);
+            if (Category == null)
+            {
+                return NotFound();
+            }
+
+            _dbContext.Category.Remove(Category);
+            await _dbContext.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool Exists(int id)
+        {
+            return (_dbContext.Category?.Any(e => e.Id == id)).GetValueOrDefault();
+        }
+    }
+}
---
+++ Controllers/ProductController.cs
@@ -0,0 +1,120 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ProductMicroservice.Models;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace ProductMicroservice.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        private readonly ProductDBContext _dbContext;
+
+        public ProductController(ProductDBContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        // GET: api/<ProductController>
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Product>>> Get()
+        {
+            if (_dbContext.Product == null)
+            {
+                return NotFound();
+            }
+            return await _dbContext.Product.ToListAsync();
+        }
+
+
+        // GET api/<ProductController>/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Product>> Get(int id)
+        {
+            if (_dbContext.Product == null)
+            {
+                return NotFound();
+            }
+            var prod = await _dbContext.Product.FindAsync(id);
+
+            if (prod == null)
+            {
+                return NotFound();
+            }
+
+            return prod;
+        }
+
+        // POST api/<ProductController>
+        [HttpPost]
+        public async Task<ActionResult<Product>> Post(Product product)
+        {
+            if (_dbContext.Product == null)
+            {
+                return Problem("Entity is null.");
+            }
+            _dbContext.Product.Add(product);
+            await _dbContext.SaveChangesAsync();
+
+            return CreatedAtAction("Get", new { id = product.Id }, product);
+        }
+
+
+        // PUT api/<ProductController>/5
+        [HttpPut("{id}")]
+        public async Task<IActionResult> Put(int id, Product product)
+        {
+            if (id != product.Id)
+            {
+                return BadRequest();
+            }
+
+            _dbContext.Entry(product).State = EntityState.Modified;
+
+            try
+            {
+                await _dbContext.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!Exists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // DELETE api/<ProductController>/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(int id)
+        {
+            if (_dbContext.Product == null)
+            {
+                return NotFound();
+            }
+            var product = await _dbContext.Product.FindAsync(id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+
+            _dbContext.Product.Remove(product);
+            await _dbContext.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool Exists(int id)
+        {
+            return (_dbContext.Product?.Any(e => e.Id == id)).GetValueOrDefault();
+        }
+    }
+}
